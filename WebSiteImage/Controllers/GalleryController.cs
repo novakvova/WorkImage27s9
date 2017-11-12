a1f34add575490542410912ebdeb85b0fdca8152
novakvova <novakvova@gmail.com>
2017-11-12 09:57:51 +0200

    Begin work add Image in site

---
+++ WebSiteImage/Controllers/GalleryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteImage.Models;
+
+namespace WebSiteImage.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(GalleryCreateViewModel galleryCreate)
+        {
+            var image = galleryCreate.Image;
+            return View();
+        }
+    }
+}
